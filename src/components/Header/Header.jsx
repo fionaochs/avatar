92d0bd6b8635642d4b35ef56da697885fe2a8b02
Fiona Ochs <fiona.oaks@gmail.com>
2020-04-29 17:03:12 -0700

    style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => (
-  <div>
+  <div style={{ color: 'teal', width: '550px', height: '50px' }}>
     <h1>Avatar the Last Airbender Characters</h1>
   </div>
 );
